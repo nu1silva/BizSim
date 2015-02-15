e96ee625f30968ca806468fb8a76a3219d257580
Nuwan Silva <nu1silva@gmail.com>
2015-02-15 20:50:36 +0530

    Day End Commit 15-Feb-2015

--- data-core/src/main/java/com/nu1silva/bizsim/datacore/h2/H2Connector.java
+++ data-core/src/main/java/com/nu1silva/bizsim/datacore/h2/H2Connector.java
@@ -17,6 +17,10 @@
  */
 package com.nu1silva.bizsim.datacore.h2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class H2Connector {
 
     /*
@@ -27,5 +31,17 @@ public class H2Connector {
 
     Configuration Handler - from Util Package.
      */
+
+    public void CreateH2Database() {
+        try {
+            Class.forName("org.h2.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/BIZSIM", "test", "");
+            Statement statement = connection.createStatement();
+
+            statement.execute("CREATE TABLE USERS");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
