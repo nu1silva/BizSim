d8547ca769ad1601839900b52d76eb5e38131214
Nuwan Silva <nu1silva@gmail.com>
2015-07-11 21:29:19 +0530

    H2 connector modifications done. added initial GameSettings.java

---
+++ data-core/src/main/java/com/nu1silva/bizsim/datacore/h2/connectors/H2Connector.java
@@ -0,0 +1,82 @@
+package com.nu1silva.bizsim.datacore.h2.connectors;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class H2Connector {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    /**
+     * Create Tables for the application
+     *
+     * @throws SQLException
+     */
+    public void createTable() throws SQLException {
+        Connection connection = getDBConnection();
+        Statement stmt = null;
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            stmt.execute("CREATE TABLE EMPLOYEE(id int primary key, fname varchar(255), lname varchar(255), salary DOUBLE, status varchar(255))");
+            stmt.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    /**
+     * Drop tables from the database
+     * NOTE: This method will probably go unused.
+     *
+     * @throws SQLException
+     */
+    public void dropTable() throws SQLException {
+        Connection connection = getDBConnection();
+        Statement stmt = null;
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            stmt.execute("DROP TABLE EMPLOYEE");
+            stmt.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+
+    /**
+     * Create Database Connection
+     */
+    public Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
+
+}
---
+++ data-core/src/main/java/com/nu1silva/bizsim/datacore/h2/domains/GameSettings.java
@@ -0,0 +1,14 @@
+package com.nu1silva.bizsim.datacore.h2.domains;
+
+public class GameSettings {
+
+    private String gameId;
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+}
