a5701dfa3ec6d900b3cc76ea5dddcc5e05887c90
Nuwan Silva <nu1silva@gmail.com>
2015-09-19 21:02:34 +0530

    Adding DB operations class

---
+++ data-core/src/main/java/com/nu1silva/bizsim/datacore/extensions/DatabaseOperations.java
@@ -0,0 +1,12 @@
+package com.nu1silva.bizsim.datacore.extensions;
+
+public class DatabaseOperations {
+
+    public void PopulateMysqlDatabase() {
+        //TODO
+    }
+
+    public void PopulateH2Database() {
+        //TODO
+    }
+}
